55639869fb9a0e2ce9820f2b1b7ea2debe4aae27
raccoonjeong <rana3782@gmail.com>
2025-04-16 13:26:41 +0900

    initial commit

---
+++ src/main/java/com/raccoon/allez_api/AllezApiApplication.java
@@ -0,0 +1,13 @@
+package com.raccoon.allez_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllezApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllezApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raccoon/allez_api/AllezApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raccoon.allez_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllezApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
